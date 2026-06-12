18a37b35de1a34e9ada96d6e8031cef9b85f4e0c
Denis <denisgrocholl@gmail.com>
2026-06-13 00:19:32 +0200

    Godot Projekt hinzugefügt

---
+++ scripts/level_selector.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_escape_pressed() -> void:
+	#Change path to that of the MainMenu Scene
+	get_tree().change_scene_to_file("res://level_selector.tscn") # Replace with function body.
+
+
+func _on_tutorial_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_subscene.tscn") # Replace with function body.
+
+
+func _on_tutorial_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_selector1.tscn") # Replace with function body.
+
+
+func _on_tutorial_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_selector2.tscn")
